5efcc5819f988b211882cefafb921fbeb13be230
Michael Walker <pythonk@email.phoenix.edu>
2025-11-28 13:35:04 -0500

    Section1: Game Setup
    Created a blank 3rd person character setup with all assets imported that I will be using for this game. The next commit will jump right into developing the character, complete with movement and basic attacks.
    As a side note, this is the first game that I will be coding with Rider. In most cases, live coding will not be possible without having to constantly shut the engine down when creating C++ scripts, which was required with Visual Studios.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Goliath
+
+Third person warrior slasher game staring Paladin Goliath as the main character.
+Developed with Unreal Engine 5.6.1 and coded with Rider 2025.3.
---
+++ Source/Goliath/Goliath.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Goliath.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Goliath, "Goliath" );
